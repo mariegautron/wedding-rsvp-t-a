b206f42b8f5d1896709728a3e9828c9ea9161f0b
Marie GAUTRON <marigaut2@gmail.com>
2023-12-31 22:54:24 +0100

    guest filters

--- components/page/GuestListView.tsx
+++ components/page/GuestListView.tsx
@@ -3,23 +3,29 @@
 import useIsAuthenticated from "@/utils/hooks/useIsAuthenticated";
 import { WeddingGuests } from "@/utils/types/weddinggests";
 import { UserOutlined, UsergroupAddOutlined } from "@ant-design/icons";
-import { Button, Space, Typography } from "antd";
+import { Button, Checkbox, Input, Space, Typography } from "antd";
 import Link from "next/link";
-import { useState } from "react";
 import LoginForm from "../LoginForm";
 import GuestListTable from "../molecules/GuestListTable";
 import StatisticList from "../molecules/StatisticList";
+import { useRef, useState } from "react";
 
 const { Title } = Typography;
+const { Search } = Input;
 
 interface GuestViewProps {
   data: WeddingGuests[];
 }
 
 const GuestView: React.FC<GuestViewProps> = ({ data }) => {
-  const [expandStats, setExpandStats] = useState(false);
-
   const isAuth = useIsAuthenticated();
+
+  const [showOnlyNotResponded, setShowOnlyNotResponded] = useState(false);
+  const [showOnlyPresent, setShowOnlyPresent] = useState(false);
+  const [showOnlyNoOneCanCome, setShowOnlyNoOneCanCome] = useState(false);
+  const [searchName, setSearchName] = useState("");
+
+  const inputRef = useRef(null);
 
   if (!isAuth) {
     return <LoginForm />;
@@ -41,6 +47,37 @@ const GuestView: React.FC<GuestViewProps> = ({ data }) => {
     { title: "Invités avec quelqu'un", value: comeWithSomeoneCount },
   ];
 
+  const guestsToShow = data.filter((guest) => {
+    if (
+      showOnlyNotResponded &&
+      (guest.isPresent === true || guest.isPresent === false)
+    ) {
+      return false;
+    }
+    if (showOnlyPresent && guest.isPresent !== true) {
+      return false;
+    }
+    if (showOnlyNoOneCanCome && guest.canComeWithSomeone === true) {
+      return false;
+    }
+
+    if (searchName.trim() !== "") {
+      const guestName = `${guest.firstname} ${guest.lastname}`.toLowerCase();
+      if (!guestName.includes(searchName.toLowerCase())) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+  const resetFilters = () => {
+    setShowOnlyNotResponded(false);
+    setShowOnlyPresent(false);
+    setShowOnlyNoOneCanCome(false);
+    setSearchName("");
+  };
+
   return (
     <div>
       <div
@@ -59,8 +96,49 @@ const GuestView: React.FC<GuestViewProps> = ({ data }) => {
           <Link href="/admin/weddingguests/add">Ajouter un invité</Link>
         </Button>
       </div>
+      <Title level={4}>Statistiques</Title>
       <StatisticList statistics={statistics} />
-      <GuestListTable data={data} />
+      <Title level={4}>Filtres</Title>
+      <div style={{ marginBottom: "24px" }}>
+        <Space direction="horizontal">
+          <Checkbox
+            checked={showOnlyNotResponded}
+            onChange={(e) => setShowOnlyNotResponded(e.target.checked)}
+          >
+            Pas encore répondu
+          </Checkbox>
+          <Checkbox
+            checked={showOnlyPresent}
+            onChange={(e) => setShowOnlyPresent(e.target.checked)}
+          >
+            Répondu - Viennent
+          </Checkbox>
+          <Checkbox
+            checked={showOnlyNoOneCanCome}
+            onChange={(e) => setShowOnlyNoOneCanCome(e.target.checked)}
+          >
+            Ne peuvent pas venir avec quelqu'un
+          </Checkbox>
+        </Space>
+      </div>
+      <Title level={4}>Recherche</Title>
+      <Search
+        ref={inputRef}
+        placeholder="Rechercher"
+        allowClear
+        enterButton
+        onSearch={(value) => setSearchName(value)}
+        style={{ marginBottom: "24px" }}
+      />
+      <Button
+        type="default"
+        onClick={resetFilters}
+        style={{ marginBottom: "24px" }}
+      >
+        Réinitialiser les filtres
+      </Button>
+      <Title level={4}>Tableaux des invités</Title>
+      <GuestListTable data={guestsToShow} />
     </div>
   );
 };
