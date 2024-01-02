38f21f67279f3affabf7f8191cfd84c098f81961
Marie GAUTRON <marigaut2@gmail.com>
2024-01-02 01:01:42 +0100

    remove weddingpresents

--- utils/constants/menuItems.ts
+++ utils/constants/menuItems.ts
@@ -1,14 +1,12 @@
 // Enum pour les clés de menu
 export enum MenuKey {
   WEDDING_GUESTS = "1",
-  WEDDING_PRESENTS = "2",
   // Ajoutez d'autres clés ici si nécessaire
 }
 
 // Enum pour les chemins de menu
 export enum MenuPath {
   WEDDING_GUESTS = "/admin/weddingguests",
-  WEDDING_PRESENTS = "/admin/weddingpresents",
   // Ajoutez d'autres chemins ici si nécessaire
 }
 
@@ -26,10 +24,6 @@ export const MENU_ITEMS: MenuItem[] = [
     path: MenuPath.WEDDING_GUESTS,
     label: "Liste des invités",
   },
-  {
-    key: MenuKey.WEDDING_PRESENTS,
-    path: MenuPath.WEDDING_PRESENTS,
-    label: "Liste de cadeaux",
-  },
+
   // Ajoutez d'autres éléments de menu si nécessaire
 ];
